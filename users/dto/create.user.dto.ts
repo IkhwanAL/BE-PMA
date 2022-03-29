4718dac90f8b59b195b686019abe7bfbd9b3ac82
ikhwan A Latif <ikhwanal235@gmail.com>
2022-03-29 16:56:17 +0700

    push rest

--- users/dto/create.user.dto.ts
+++ users/dto/create.user.dto.ts
@@ -1,8 +1,8 @@
 export interface CreateUserDto {
-    id?: number;
     email: string;
     username: string;
     password: string;
+    phoneNumber?: string;
     firstName?: string;
     lastName?: string;
     link?: string;
